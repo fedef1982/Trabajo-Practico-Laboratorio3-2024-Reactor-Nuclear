687588ca98a574f4793b444ae25b319a54a2841e
fedef1982 <fedef1982@gmial.com>
2024-05-27 10:59:46 -0300

    Modifico clase tablero

--- src/tablero/tablero.ts
+++ src/tablero/tablero.ts
@@ -1,7 +1,7 @@
 import ColeccionTemperaturasVaciasException from "../exceptions/coleccionTemperaturasVaciasException";
 import Sensor from "../reactor/sensor";
 
-// 1) 5) y 6)
+// Resuelve enunciado 1) 5) y 6)
 
 export default class Tablero{
     private _temperaturas : number[];
