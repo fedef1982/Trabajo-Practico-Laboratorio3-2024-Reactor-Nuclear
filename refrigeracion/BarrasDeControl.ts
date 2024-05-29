8f023fb4e78f4c2a6f518ae1fdef8d41dd678ed2
NiAlGon <nicolasalbertogonzalez@hotmail.com>
2024-05-28 21:18:32 -0300

    Modifico Barras

---
+++ refrigeracion/BarrasDeControl.ts
@@ -0,0 +1,41 @@
+import Refrigerable from "./Refrigerable";
+import Reactor from "../reactor/reactor";
+
+export default class BarrasDeControl implements Refrigerable{
+    private _porcentajeReduccion : number;
+    private _tiempoVidaUtil : number;
+
+    constructor(tiempoVidaUtil? : number){
+        this._porcentajeReduccion = -1;
+        this._tiempoVidaUtil = -1;
+        if (tiempoVidaUtil != undefined) {
+            this._tiempoVidaUtil = tiempoVidaUtil;
+        }
+    }
+    
+    public get tiempoVidaUtil() : number {
+        return this._tiempoVidaUtil;
+    }
+    
+    public set tiempoVidaUtil(tiempo : number){
+        this._tiempoVidaUtil = tiempo;
+    }    
+
+    public get porcentajeReduccion() : number {
+        return this._porcentajeReduccion;
+    }
+    
+    public actualizar() : void{
+       this._porcentajeReduccion = (this._tiempoVidaUtil /3600) * 100;
+       this.restarVidaUtil();
+    }
+
+    public restarVidaUtil () : void{
+        this._tiempoVidaUtil--;
+    }
+
+    disminuirTemperatura(reactor : Reactor){
+        this.actualizar();
+        reactor.disminuirEnergia(this._porcentajeReduccion);
+    }
+}
