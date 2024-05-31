5829b86527ba168c8c2d9c7105df0907900841db
Abril_Alvarez <117129041+abrilAlvarez1@users.noreply.github.com>
2024-05-31 23:59:26 +0000

    genero getters y setters

--- src/tablero/mensaje.ts
+++ src/tablero/mensaje.ts
@@ -11,6 +11,18 @@ export default class Mensaje{
     public get mensaje() : string {
         return this._mensaje;
     }
+
+    public get leido() : boolean {
+        return this._leido;
+    }
+
+    public set leido(leido : boolean) {
+        this._leido = leido;
+    }
+
+    public set mensaje(mensaje : string) {
+        this._mensaje = mensaje;
+    }
     
 
 }
