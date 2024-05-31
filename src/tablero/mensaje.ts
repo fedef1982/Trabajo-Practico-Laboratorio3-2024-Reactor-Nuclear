98e3383030116e9525c00566632e92b2fc300781
Abril_Alvarez <117129041+abrilAlvarez1@users.noreply.github.com>
2024-05-31 23:56:23 +0000

    genero constructor

--- src/tablero/mensaje.ts
+++ src/tablero/mensaje.ts
@@ -1,4 +1,10 @@
 export default class Mensaje{
     private _leido : boolean;
     private _mensaje : string;
+
+    constructor(mensaje : string){
+        this._mensaje = mensaje ?? "";
+        this._leido = false;
+    }
+
 }
