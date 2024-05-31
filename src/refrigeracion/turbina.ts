7e0f02fe1267e569f43bf64add731628834cc30a
NiAlGon <nicolasalbertogonzalez@hotmail.com>
2024-05-30 21:05:33 -0300

    Agrego turbina

---
+++ src/refrigeracion/turbina.ts
@@ -0,0 +1,32 @@
+import Refrigerable from "./refrigerable";
+
+export default class Turbina implements Refrigerable{
+    private _porcentajeReduccion : number;
+    private _activo : boolean;
+    
+    constructor(){
+        this._porcentajeReduccion = -1;
+        this._activo = false;
+    }
+
+    public activar(){
+        this._activo = true;
+    }
+
+    public desactivar(){
+        this._activo = false;
+    }
+
+    
+    public get porcentajeReduccion() : number {
+        return this._porcentajeReduccion;
+    }
+    
+    esUtil(): boolean {
+        return this._activo;
+    }
+
+    disminuirTemperatura() {
+        return this._porcentajeReduccion;
+    }
+}
