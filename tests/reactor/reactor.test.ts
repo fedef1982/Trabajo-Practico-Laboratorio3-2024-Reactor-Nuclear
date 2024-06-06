de6cf7b629d7bf9b6571f3c58e82b158f77afcd1
camila garcia <camirolejoooo@gmail.com>
2024-06-05 23:02:18 -0300

    Modifico tests del reactor

--- tests/reactor/reactor.test.ts
+++ tests/reactor/reactor.test.ts
@@ -1,23 +1,27 @@
 import ICombustible from "../../src/reactor/ICombustible";
 import IEstadoReactor from "../../src/reactor/IEstadoReactor";
+import INucleo from "../../src/reactor/INucleo";
 import ISensor from "../../src/reactor/ISensor";
 import Reactor from "../../src/reactor/reactor";
 import * as MOCKS from "../mocks";
 
 
-describe("Teste a la clase reactor", () => {
+describe("Testea la clase reactor", () => {
     let instance : Reactor;
     let mockSensor : ISensor;
     let mockEstadoManager : IEstadoReactor;
-    let combustible : ICombustible;
+    let mockCombustible : ICombustible;
+    let mockNucleo : INucleo;
+    let capacidad : number;
 
-
-   
     beforeEach( () => {
-        combustible = MOCKS.MockCombustible;
         mockSensor = MOCKS.MockSensor;
         mockEstadoManager = MOCKS.MockEstadoManager;
-        instance = new Reactor(mockSensor, combustible, mockEstadoManager);
+        mockCombustible = MOCKS.MockCombustible;
+        mockNucleo = MOCKS.MockNucleo;
+        capacidad = 700;
+
+        instance = new Reactor(capacidad, mockCombustible, mockSensor, mockEstadoManager ,mockNucleo);
     })
 
     describe("Testea el método iniciar()", () => {
@@ -31,5 +35,47 @@ describe("Teste a la clase reactor", () => {
             // Verifica que la temperatura del reactor haya aumentado
             expect(instance.temperatura).toBeGreaterThan(temperaturaInicial);
         });
+
+        it("Al iniciar se debería generar energía hasta que se acabe el combustible.", () => {
+            mockCombustible.cantidadCombustible = 2;
+
+            instance.iniciar();
+
+            expect(mockCombustible.getCantidadCombustible).toBe(0);
+        }) 
     });
+
+    describe("Testea el método detener()", () => {
+        it("La temperatura debería ser 1 cuando el reactor se detiene.", () => {
+            instance.iniciar();
+
+            instance.detener();
+            expect(instance.temperatura).toBe(1);
+        })
+    });
+
+    describe("Testea el método disminuirEnergia()", () => {
+        
+        it("Con un porcentaje de reducción de 20 (20%) y una temperatura actual de 100, la temperatura debería disminuir a 80.", () => {
+            mockSensor.temperaturaReactor = 100;
+            instance.disminuirEnergia(20);
+
+            expect(instance.temperatura).toBe(80);
+        })
+
+        it("Con un porcentaje de reducción de 2 (2%) y una temperatura actual de 10, la temperatura debería ser 9.8", () => {
+            mockSensor.temperaturaReactor = 10;
+            instance.disminuirEnergia(2);
+
+            expect(instance.temperatura).toBe(9.8);
+        })
+    });
+
+    describe("Testea que los atributos se seteen de forma correcta.", () => {
+        it("testeo el setter para la capacidad", () => {
+            instance.capacidad = 7;
+            expect(instance.capacidad).toBe(7);
+        })
+    })
+
 })
