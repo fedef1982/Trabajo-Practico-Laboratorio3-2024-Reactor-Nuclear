8dcf0ea787e63442e93f51a7723e5511526191d9
camila garcia <camirolejoooo@gmail.com>
2024-06-05 22:24:46 -0300

    agrego array de suscriptores y métodos relacionados con el patrón observer

--- src/reactor/sensor.ts
+++ src/reactor/sensor.ts
@@ -1,17 +1,36 @@
 import ISensor from "./ISensor";
+import ISuscriptorTemperatura from "./ISuscriptorTemperatura";
 
 export default class Sensor implements ISensor{
     private _temperaturaReactor : number;
+    private _suscriptores : ISuscriptorTemperatura[];
 
     constructor() {
         this._temperaturaReactor = 1;//temperatura inicial
+        this._suscriptores = [];
     }
 
     public set temperaturaReactor(temperatura : number) {
         this._temperaturaReactor = temperatura;
+        this.notificar();
     }
+    
     public get getTemperaturaReactor () : number {
         return this._temperaturaReactor;
+    }
+
+    public suscribir(suscriptor : ISuscriptorTemperatura) {
+        this._suscriptores.push(suscriptor);
+    }
+
+    public desuscribir(suscriptor : ISuscriptorTemperatura) {
+        this._suscriptores = this._suscriptores.filter( (suscrip) => suscrip !== suscriptor);
+    }
+
+    private notificar() : void {
+        this._suscriptores.forEach( (suscriptor) => {
+            suscriptor.actualizarEstado(this._temperaturaReactor);
+        })
     }
 
 }
