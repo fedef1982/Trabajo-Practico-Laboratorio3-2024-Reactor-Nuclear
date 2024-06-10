9eb15a93ccfcaf70cd6ffc007485426efcb7a156
Abril_Alvarez <117129041+abrilAlvarez1@users.noreply.github.com>
2024-06-10 19:51:57 +0000

    quito imports y metodos que ya no sirven

--- src/reactor/reactor.ts
+++ src/reactor/reactor.ts
@@ -1,22 +1,18 @@
 import {EstadoReactor} from "../enums/estadoReactor";
 import ICombustible from "./ICombustible";
 import IEstadoReactor from "./IEstadoReactor";
-import INucleo from "./INucleo";
-import ISensor from "./ISensor";
 import Nucleo from "./nucleo";
 
 
 export default class Reactor{
     private _capacidad : number;
     private _combustible : ICombustible;
-    private _sensor : ISensor;
     private _estado : EstadoReactor;
     private _nucleo : Nucleo;
 
-    constructor(capacidad : number, combustible : ICombustible, sensor : ISensor, nucleo : Nucleo){
+    constructor(capacidad : number, combustible : ICombustible,  nucleo : Nucleo){
         this._capacidad = capacidad;
         this._combustible = combustible;
-        this._sensor = sensor;
         this._estado = new EstadoReactorApagado();
         this._nucleo = nucleo;
     }
@@ -25,12 +21,9 @@ export default class Reactor{
         return  this._capacidad;
     }
 
-    public get temperatura() : number {
-        return  this._sensor.getTemperaturaReactor;
-    }
 
     public get estado() : EstadoReactor {
-        return  this._estadoReactorManager.estado;
+        return  this._estado;
     }
 
     public get nucleo() : Nucleo{
@@ -43,9 +36,7 @@ export default class Reactor{
     public set combustible(combustible : ICombustible){
         this._combustible = combustible;
     }
-    public set sensor(sensor : ISensor){
-        this._sensor = sensor;
-    }
+   
     /*
     ---------SE VA--------------
     public set estadoManager(estadoManager : IEstadoReactor){
@@ -67,11 +58,13 @@ export default class Reactor{
         }
     }
 
+    /*
+    ------ MODIFICAR-----
     public detener() {
         this._sensor.temperaturaReactor = 1;
     }
 
-    /*
+    
     -----------------SE VA----------
     public disminuirEnergia(porcentajeReduccion : number) {//baja la temperatura
         let temperatura = this._sensor.getTemperaturaReactor;
