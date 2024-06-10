c063210e18f52cd4bd119e31be348fb34ef4b546
Abril_Alvarez <117129041+abrilAlvarez1@users.noreply.github.com>
2024-06-10 19:35:09 +0000

    modifico reactor, agrego nucleo

--- src/reactor/reactor.ts
+++ src/reactor/reactor.ts
@@ -3,20 +3,21 @@ import ICombustible from "./ICombustible";
 import IEstadoReactor from "./IEstadoReactor";
 import INucleo from "./INucleo";
 import ISensor from "./ISensor";
+import Nucleo from "./nucleo";
 
 
 export default class Reactor{
     private _capacidad : number;
     private _combustible : ICombustible;
     private _sensor : ISensor;
-    private _estadoReactorManager : IEstadoReactor;
-    private _nucleo : INucleo;
+    private _estado : EstadoReactor;
+    private _nucleo : Nucleo;
 
-    constructor(capacidad : number, combustible : ICombustible, sensor : ISensor, estadoManager : IEstadoReactor, nucleo : INucleo){
+    constructor(capacidad : number, combustible : ICombustible, sensor : ISensor, nucleo : Nucleo){
         this._capacidad = capacidad;
         this._combustible = combustible;
         this._sensor = sensor;
-        this._estadoReactorManager = estadoManager;
+        this._estado = new EstadoReactorApagado();
         this._nucleo = nucleo;
     }
 
@@ -32,6 +33,10 @@ export default class Reactor{
         return  this._estadoReactorManager.estado;
     }
 
+    public get nucleo() : Nucleo{
+        return this._nucleo;
+    }
+
     public set capacidad(capacidad : number){
         this._capacidad = capacidad;
     }
@@ -41,14 +46,20 @@ export default class Reactor{
     public set sensor(sensor : ISensor){
         this._sensor = sensor;
     }
+    /*
+    ---------SE VA--------------
     public set estadoManager(estadoManager : IEstadoReactor){
         this._estadoReactorManager = estadoManager;
     }
     public set nucleo(nucleo : INucleo){
         this._nucleo = nucleo;
-    }
+    }*/
 
     //°---Métodos-------------->
+
+    public equals(object : EstadoReactor) : boolean {
+        return this._estado instanceof object;
+    }
 
     public iniciar() {//comienza a generar energia hasta que se acabe el combustible
         while(this._combustible.getCantidadCombustible > 0) {
@@ -60,13 +71,15 @@ export default class Reactor{
         this._sensor.temperaturaReactor = 1;
     }
 
+    /*
+    -----------------SE VA----------
     public disminuirEnergia(porcentajeReduccion : number) {//baja la temperatura
         let temperatura = this._sensor.getTemperaturaReactor;
 
         temperatura -= ((temperatura * porcentajeReduccion) / 100);
 
         this._sensor.temperaturaReactor = temperatura;
-    }
+    }*/
 
     private generarEnergia() {
         let temperatura : number = this._sensor.getTemperaturaReactor;
