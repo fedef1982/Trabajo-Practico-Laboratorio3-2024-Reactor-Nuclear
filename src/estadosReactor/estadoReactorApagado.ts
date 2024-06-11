3bc70e606aa5efd9dc48177c78338d2bcb6bf53c
NiAlGon <nicolasalbertogonzalez@hotmail.com>
2024-06-10 21:28:17 -0300

    Agrego el metodo actualizar(faltante)

--- src/estadosReactor/estadoReactorApagado.ts
+++ src/estadosReactor/estadoReactorApagado.ts
@@ -2,6 +2,9 @@ import Reactor from "../reactor/reactor";
 import EstadoReactor from "./estadoReactor";
 
 export default class EstadoReactorApagado extends EstadoReactor{
+    public actualizarEstado(estado: EstadoReactor): void {
+        throw new Error("No se puede actualizar");
+    }
     
     public generarEnergia(): void {
         throw new Error("no genera energia xq esta apagado");
