0d6cb25cbb4ccfdfcb877d8bdb07756db542ac4c
NiAlGon <nicolasalbertogonzalez@hotmail.com>
2024-06-10 21:27:05 -0300

    Agrego constructor, valor de retorno y parametro en actualizar

--- src/estadosReactor/estadoReactor.ts
+++ src/estadosReactor/estadoReactor.ts
@@ -3,6 +3,10 @@ import Reactor from "../reactor/reactor";
 export default abstract class EstadoReactor{
     protected _reactor : Reactor;
 
-    public abstract generarEnergia();
-    public abstract actualizarEstado();
+    constructor(reactor: Reactor) {
+        this._reactor = reactor;
+    }
+
+    public abstract generarEnergia() : void;
+    public abstract actualizarEstado(estado : EstadoReactor) : void;
 }
