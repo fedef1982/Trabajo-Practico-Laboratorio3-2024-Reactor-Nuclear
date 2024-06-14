70934457d77f47339ffae50bdc098cfc7454b3b0
NiAlGon <nicolasalbertogonzalez@hotmail.com>
2024-06-14 14:37:54 -0300

    Se elimina import no implementado y metodo actualizar, tambien se cambia la implementacion de generar y su parametro

--- src/estadosReactor/estadoReactorEncendido.ts
+++ src/estadosReactor/estadoReactorEncendido.ts
@@ -1,6 +1,5 @@
 import Reactor from "../reactor/reactor";
 import EstadoReactor from "./estadoReactor";
-import EstadoReactorDisminuido from "./estadoReactorDisminuido";
 import EstadoReactorNormal from "./estadoReactorNormal";
 
 export default class EstadoReactorEncendido extends EstadoReactor{
@@ -8,38 +7,34 @@ export default class EstadoReactorEncendido extends EstadoReactor{
     constructor(reactor : Reactor){
         super(reactor);
     }
-
-    public actualizarEstado(estado: EstadoReactor): void {
-        this._reactor.estado = estado;
+    
+    private calentar() : void{
+        if (this._reactor.combustible.getCantidadCombustible > 0) {
+            const combustibleActual : number = this._reactor.combustible.getCantidadCombustible;
+            this._reactor.combustible.cantidadCombustible = (combustibleActual - 1);
+        }        
     }
 
-    public generarEnergia(): void {
+    public generarEnergia(horasParaGenerarEnergia : number): void {
 
         //creo variable para tener la temperatura del reactor,hay que pedirle la temperatura al nucleo??
-        let temperaturaReactorActual : number = this._reactor.sensor.getTemperaturaReactor;
-
+        let temperaturaReactorActual : number = this._reactor.nucleo.sensor.getTemperaturaReactor;
+        let horasQueLlevaGenerando : number = 1;
         //caliento el reactor hasta empezar a generar energia
-        while(temperaturaReactorActual < 280){
+        while(temperaturaReactorActual < 280 && horasQueLlevaGenerando <= horasParaGenerarEnergia){
+            //Consume combustible al calentar el nucleo
+            this.calentar();
 
-            //calcula la temperatura a aumentar
-            temperaturaReactorActual += temperaturaReactorActual * (this._reactor.combustible.porcentajeAumentoTemperatura / 100);
-            //seteo la temperatura al reactor, aca deberia ser al nucleo??
-            this._reactor.sensor.temperaturaReactor = temperaturaReactorActual;
-
-            //guardo en una variable la cant de combustible
-            let combustibleActual = this._reactor.combustible.getCantidadCombustible;
-    
-            //actualizo al reactor su combustible restandole 1
-            this._reactor.combustible.cantidadCombustible = combustibleActual - 1;
-
-            //actualizo la variable de temperatura del reactor, la cual uso para mi bucle while
-            temperaturaReactorActual = this._reactor.sensor.getTemperaturaReactor;
-
+            temperaturaReactorActual += temperaturaReactorActual + 1;
+            this._reactor.nucleo.temperatura = temperaturaReactorActual;
+            horasQueLlevaGenerando++;
         }
-
+        
+        let horasRestantes : number = horasParaGenerarEnergia - horasQueLlevaGenerando;
         //cuando se calento el reactor empiezo a generar energiar pasandolo a normal
-        if(this._reactor.sensor.getTemperaturaReactor >= 280 && this._reactor.sensor.getTemperaturaReactor < 330){
+        if(this._reactor.nucleo.sensor.getTemperaturaReactor >= 280 && this._reactor.nucleo.sensor.getTemperaturaReactor < 330){
             this.actualizarEstado(new EstadoReactorNormal(this._reactor));
+            this._reactor.estado.generarEnergia(horasRestantes);
         }
     }
 
