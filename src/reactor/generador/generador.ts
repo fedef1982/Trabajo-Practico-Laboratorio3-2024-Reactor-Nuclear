6f43b5879c20049f45342c33c3e104d066c49479
camila garcia <camirolejoooo@gmail.com>
2024-06-15 18:30:28 -0300

    Agrego el generador de energía

---
+++ src/reactor/generador/generador.ts
@@ -0,0 +1,24 @@
+import IGenerador from "./iGenerador";
+
+export default class generador implements IGenerador {
+    private _energiaProducida : number;
+
+    constructor() {
+        this._energiaProducida = 0;
+    }
+
+    public generarEnergia(capacidadProductiva: number, temperatura: number): number {
+        let energiaProducida = ((this._calcularEnergiaNeta(temperatura)) / 100 ) * capacidadProductiva;
+        this._energiaProducida = energiaProducida;
+        return energiaProducida;
+    }
+
+    public get energiaProducida() : number {
+        return this._energiaProducida;
+    }
+    
+    private _calcularEnergiaNeta(temperatura : number): number{
+        if (temperatura > 280 && temperatura < 287.141 ) return 100;
+        return (14 * temperatura) - 3919.97;
+    }
+}
