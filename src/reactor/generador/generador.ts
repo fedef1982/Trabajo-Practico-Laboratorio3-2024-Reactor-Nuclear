8e34658e2f5d257a6695d79a2b8a477edb6f2528
camila garcia <camirolejoooo@gmail.com>
2024-06-15 19:11:51 -0300

    Modificaciones

--- src/reactor/generador/generador.ts
+++ src/reactor/generador/generador.ts
@@ -16,9 +16,10 @@ export default class generador implements IGenerador {
     public get energiaProducida() : number {
         return this._energiaProducida;
     }
-    
+
     private _calcularEnergiaNeta(temperatura : number): number{
-        if (temperatura > 280 && temperatura < 287.141 ) return 100;
-        return (14 * temperatura) - 3919.97;
+        if(temperatura < 280) return 0;
+        if (temperatura >= 280 && temperatura < 287.141 ) return 100;
+        return parseFloat(((14 * temperatura) - 3919.97).toFixed(2));
     }
 }
