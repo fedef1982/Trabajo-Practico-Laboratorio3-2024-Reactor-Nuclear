ed4f3f3879f9f9ed25eba5d18d26d9d907dd8302
NiAlGon <nicolasalbertogonzalez@hotmail.com>
2024-06-19 19:08:43 -0300

    Se inicializa variable estado

--- src/tablero/alerta.ts
+++ src/tablero/alerta.ts
@@ -1,7 +1,7 @@
 import EstadoReactor from "../estadosReactor/estadoReactor";
 
 export default class Alerta{
-    private _estado : EstadoReactor;
+    private _estado : EstadoReactor = undefined as unknown as EstadoReactor;
     
     public set estado(estado : EstadoReactor) {
         this._estado = estado;
