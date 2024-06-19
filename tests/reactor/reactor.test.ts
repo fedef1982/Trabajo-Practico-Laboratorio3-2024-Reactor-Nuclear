978385e8f7abf9f47f064f687559305984eaba28
camila garcia <camirolejoooo@gmail.com>
2024-06-19 12:22:13 -0300

    Actualizo test del reactor

--- tests/reactor/reactor.test.ts
+++ tests/reactor/reactor.test.ts
@@ -32,4 +32,52 @@ describe('Reactor', () => {
         expect(instance.estado).toBeInstanceOf(EstadoReactorApagado);
     });
 
+
+    describe("Testea los getters y setters", () => {
+
+        it('debería obtener la capacidad correctamente', () => {
+            expect(instance.capacidad).toBe(700);
+        });
+        
+        it('debería permitir cambiar la capacidad correctamente', () => {
+            instance.capacidad = 1500;
+            expect(instance.capacidad).toBe(1500);
+        });
+        
+        it('debería obtener el estado inicializado correctamente', () => {
+            expect(instance.estado).toBeInstanceOf(EstadoReactorApagado);
+        });
+        
+        it('debería permitir cambiar el estado correctamente', () => {
+            const nuevoEstado = MOCKS.mockEstadoReactorEncendido;
+            instance.estado = nuevoEstado;
+            expect(instance.estado).toBe(nuevoEstado);
+        });
+        
+        it('debería obtener el núcleo correctamente', () => {
+            expect(instance.nucleo).toBe(mockNucleo);
+        });
+        
+        it('debería obtener el combustible correctamente', () => {
+            expect(instance.combustible).toBe(mockCombustible);
+        });
+        
+        it('debería permitir cambiar el combustible correctamente', () => {
+            const nuevoCombustible: ICombustible = {
+              cantidadCombustible: 200,
+              porcentajeAumentoTemperatura: 20,
+            };
+            instance.combustible = nuevoCombustible;
+            expect(instance.combustible).toBe(nuevoCombustible);
+        });
+        
+        it('debería obtener la energía producida correctamente', () => {
+            expect(instance.energiaProducida).toBe(0);
+        });
+        
+        it('debería permitir cambiar la energía producida correctamente', () => {
+            instance.energiaProducida = 500;
+            expect(instance.energiaProducida).toBe(500);
+        });
+    })
 });
