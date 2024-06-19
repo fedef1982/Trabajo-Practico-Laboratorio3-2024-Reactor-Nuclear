9c75aa83f0fbdafbc6e68ab39c1db6b218315275
NiAlGon <nicolasalbertogonzalez@hotmail.com>
2024-06-19 15:02:14 -0300

    Burns pasa a ser singleton

--- src/operador/srBurns.ts
+++ src/operador/srBurns.ts
@@ -3,12 +3,20 @@ import EstadoReactor from "../estadosReactor/estadoReactor";
 import Alerta from "../tablero/alerta";
 
 export default class SrBurns implements ISuscriptorEstadoApagado{
+    private static instance : SrBurns;
     private _alertas : Alerta[];
 
     constructor(){
         this._alertas = undefined as unknown as Alerta[];
     }
     
+    public static getInstance(): SrBurns {
+        if (SrBurns.instance === null) {
+            SrBurns.instance = new SrBurns();
+        }
+        return SrBurns.instance;
+    }
+
     recibirAlerta(estado: EstadoReactor) {
         let alerta = new Alerta();
         alerta.estado = estado;
