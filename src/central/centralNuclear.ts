0a7d40f1a6e6205a19350a4715f5eb6b2499bb8c
NiAlGon <nicolasalbertogonzalez@hotmail.com>
2024-06-19 11:51:41 -0300

    Agrego constructor y relacionado a tablero

--- src/central/centralNuclear.ts
+++ src/central/centralNuclear.ts
@@ -1,13 +1,27 @@
 import Operador from "../operador/operador";
 import Reactor from "../reactor/reactor";
 import RefrigerableStrategy from "../refrigeracion/refrigerableStrategy";
+import Tablero from "../tablero/tablero";
 
 export default class CentralConStrategy {
     
     private _reactor : Reactor = undefined as unknown as Reactor;
     private _operadores : Operador[] = [];
     private _strategy : RefrigerableStrategy = undefined as unknown as RefrigerableStrategy;
+    private _tablero : Tablero = undefined as unknown as Tablero;
   
+    constructor(reactor : Reactor, tablero : Tablero){
+        this._reactor = reactor;
+        this._tablero = tablero;
+    }
+
+    public set tablero(tablero : Tablero) {
+        this._tablero = tablero;
+    }
+    
+    public get tablero() : Tablero {
+        return this._tablero;
+    }
 
     public set strategy(strategy : RefrigerableStrategy){
         this._strategy = strategy;
