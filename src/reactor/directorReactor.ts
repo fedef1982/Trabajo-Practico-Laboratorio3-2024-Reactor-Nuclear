b9ed69323691c3f05b1f9ab3b1d51401dcfd9506
camila garcia <camirolejoooo@gmail.com>
2024-06-18 21:47:44 -0300

    modificaciones

--- src/reactor/directorReactor.ts
+++ src/reactor/directorReactor.ts
@@ -2,7 +2,7 @@ import { CANTIDAD_COMBUSTIBLE_INICIAL, CAPACIDAD_MAX_REACTOR, PORCENTAJE_AUMENTO
 import IBuilderReactor from "./IBuilderReactor";
 import Combustible from "./combustible";
 import Nucleo from "./nucleo";
-import Generador from "./Generador";
+import Generador from "./generador/Generador"
 
 
 export default class DirectorReactor {
@@ -19,7 +19,7 @@ export default class DirectorReactor {
     public crearReactor() : void {
         this._builder.setCapacidad(CAPACIDAD_MAX_REACTOR);
         this._builder.setCombustible(new Combustible(CANTIDAD_COMBUSTIBLE_INICIAL, PORCENTAJE_AUMENTO_TEMPERATURA_REACTOR));
-        this._builder.setNucleo(new Nucleo()); // revisar! por que no implementa la interfaz??
+        this._builder.setNucleo(new Nucleo());
         this._builder.setGenerador(new Generador());
     }
 
