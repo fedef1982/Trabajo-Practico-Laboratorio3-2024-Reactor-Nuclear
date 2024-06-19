e4bbbf3dd8a9d53502f88f8d66ea4966fe412904
camila garcia <camirolejoooo@gmail.com>
2024-06-18 22:20:35 -0300

    mocks

--- tests/mocks.ts
+++ tests/mocks.ts
@@ -1,40 +1,12 @@
-import { EstadoReactor } from "../src/enums/estadoReactor";
-import ICombustible from "../src/reactor/ICombustible";
-import INucleo from "../src/reactor/INucleo";
-
-export const MockSensor = {
-    _temperaturaReactor : 1,
-
-    get getTemperaturaReactor(){
-        return this._temperaturaReactor;
-    } ,
-    set temperaturaReactor(temperatura : number) {
-        this._temperaturaReactor = temperatura;
-    },
-};
-
-export const MockEstadoManager = {
-    actualizarEstado: jest.fn(),
-    estado: EstadoReactor.ENCENDIDO
-};
-
-
-export const MockCombustible = {
-    _cantCombustible : 10,
-    _porcentajeAumento : 20,
+// Mocking the dependencies
+export const mockCombustible = {
     
-    set cantidadCombustible(cantCombustible : number) {
-        this._cantCombustible = cantCombustible;
-    },
-    get getCantidadCombustible() : number {
-        return this._cantCombustible;
-    },
-    get porcentajeAumentoTemperatura() : number {
-        return this._porcentajeAumento;
-    }
-};
+}as any;
 
-export const MockNucleo : INucleo = {
-    insertarBarraDeControl: jest.fn(),
-    obtenerBarraDeControl: jest.fn()
-}
+export const mockNucleo = {
+    
+}as any;
+
+export const mockGenerador = {
+    
+}as any;
