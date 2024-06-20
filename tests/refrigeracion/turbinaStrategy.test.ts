7e178cf27606693240ca74ac9ff405e4250b280b
Abril_Alvarez <117129041+abrilAlvarez1@users.noreply.github.com>
2024-06-20 20:57:03 +0000

    implementacion test turbinaStrategy

--- tests/refrigeracion/turbinaStrategy.test.ts
+++ tests/refrigeracion/turbinaStrategy.test.ts
@@ -2,42 +2,57 @@ import INucleo from "../../src/reactor/INucleo";
 import ISensor from "../../src/reactor/ISensor";
 import Reactor from "../../src/reactor/reactor";
 import Turbina from "../../src/refrigeracion/turbina";
+import Nucleo from "../../src/reactor/nucleo";
 import TurbinaStrategy from "../../src/refrigeracion/turbinaStrategy";
 import * as MOCKS from "../mocks";
+import Sensor from "../../src/reactor/sensor";
 
-describe("Turbina", () => {
+describe("Turbina strategy", () => {
 
-    let instance: TurbinaStrategy;
+    let instance : TurbinaStrategy;
+    
     let mockTurbina : Turbina;
+    let mockTurbina2 : Turbina;
     let mockReactor : Reactor;
     let mockSensor : ISensor;
     let mockNucleo : INucleo;
-  
+ 
     beforeEach(() => {
         mockTurbina = MOCKS.mockTurbina;
-        mockReactor = MOCKS.mockReactor;
-        mockSensor = MOCKS.mockSensor;
-        mockNucleo = MOCKS.mockNucleo;
-        instance = new TurbinaStrategy([mockTurbina]);
+        mockTurbina2 = MOCKS.mockTurbina;
+        mockReactor = MOCKS.mockReactor2;
+        //mockSensor = MOCKS.mockSensor;
+        //mockNucleo = MOCKS.mockNucleo2;
+        
+        instance = new TurbinaStrategy([mockTurbina, mockTurbina2]);
+        mockReactor = MOCKS.mockReactor2;
     });
 
     afterEach(() => {
         jest.clearAllMocks();
     });
+    
   
-    it("Test metodo disminuirTemperatura() de 100 a 50", () => {
-        //mockReactor.nucleo.temperatura = 100;
-        //mockReactor.nucleo.sensor.temperaturaNucleo = 100;
-
-
+    it("Test disminuirTemperatura() de 100 a 90", () => {
+    
+        
         instance.disminuirTemperatura(10,mockReactor);
 
-        let temperatura = mockNucleo.temperatura;
-
-        expect(temperatura).toBe(90);
+        expect(mockReactor.nucleo.temperatura).toBe(90);
+  
 
     });
+
+    it("Test enfriar() con varias turbinas de 100 a 81", () => {
     
+        const disminuir = jest.spyOn(instance,"disminuirTemperatura");
+
+        instance.enfriar(mockReactor);
+
+        expect(disminuir).toHaveBeenCalledTimes(2);
+        
+
+    });
     
 
   });
